0ef08ac3e0bafb4fe3ec754c2d9d2874fff05592
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-14 18:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-function Scooter ('year', 'color', 'model') {
-  this.year = year;
+function Scooter(year, color, model){
+  this.year = year; 
   this.color = color;
   this.model = model;
-
 }
-let samson = new Scooter(2015, 'red', 'swoosh');
